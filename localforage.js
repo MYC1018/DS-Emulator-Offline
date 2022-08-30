8a8dbe077070fc4499a581b0326d21d5e8089162
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2022-08-30 09:00:31 +0900

    Add files via upload

--- localforage.js
+++ localforage.js
@@ -1,15 +1,3 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
-
 /*!
     localForage -- Offline Storage, Improved
     Version 1.7.3
@@ -2582,7 +2570,7 @@ var LocalForage = function () {
         var promise = new Promise$1(function (resolve, reject) {
             try {
                 var driverName = driverObject._driver;
-                var complianceError = new Error('Custom driver not compliant; see ' + 'https://web.archive.org/web/20220826025609/https://mozilla.github.io/localForage/#definedriver');
+                var complianceError = new Error('Custom driver not compliant; see ' + 'https://mozilla.github.io/localForage/#definedriver');
 
                 // A driver name should be defined and not overlap with the
                 // library-defined, default drivers.
@@ -2807,26 +2795,3 @@ module.exports = localforage_js;
 
 },{"3":3}]},{},[4])(4)
 });
-
-
-}
-/*
-     FILE ARCHIVED ON 02:56:09 Aug 26, 2022 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 09:42:57 Aug 26, 2022.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 199.726
-  exclusion.robots: 0.152
-  exclusion.robots.policy: 0.136
-  RedisCDXSource: 4.391
-  esindex: 0.012
-  LoadShardBlock: 176.418 (3)
-  PetaboxLoader3.datanode: 62.424 (4)
-  CDXLines.iter: 15.344 (3)
-  load_resource: 15.939
-*/
