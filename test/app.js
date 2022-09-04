fe8135b91f0dc0aa9c452d31c784f398d38c7fe0
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2022-09-04 20:03:12 +0900

    Update app.js

--- test/app.js
+++ test/app.js
@@ -925,6 +925,19 @@ var gamePadKeyMap = {
     'left': 14,
     'right': 15
 }
+
+if (isSaveSupported) {
+    window.addEventListener("gamepadconnected", function (e) {
+        console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+            e.gamepad.index, e.gamepad.id,
+            e.gamepad.buttons.length, e.gamepad.axes.length);
+        showMsg('Gamepad connected.')
+        currentConnectedGamepad = e.gamepad.index
+        $id('a-gamepad').innerText = 'Gamepad connected'
+    });
+}
+
+
 function whatsNew() {
     alert(`
 1. Improve auto-save, export/import save.
