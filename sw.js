1890fea4f0e12eb63843bd9597aaa6c5bfca5f55
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2022-09-27 09:36:27 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,82 @@
-// Cache name
-const CACHE_NAME = 'pwa-sample-caches-v1';
-// Cache targets
-const urlsToCache = [
-  './',
-  './index.html',
-  './audio-worklet.js',
-  './dark.css',
-  './icon.png',
-  './localforage.js',
-  './manifest.json',
-  './nds.js',
-  './nds.wasm',
+var CACHE_NAME = 'v43';
+
+
+var urlsToCache = [
+    '/',
+    '/app.js',
+    '/localforage.js',
+    '/pako.min.js',
+    '/manifest.json',
+    '/dark.css',
+    '/favicon.ico',
+    '/icon.png',
+    '/build/nds.js',
+    '/build/nds.wasm',
+    /*
+    '/gba/a.out.js',
+    '/gba/a.out.wasm',
+    '/gba/icon.png',
+    '/gba/',
+    '/gba/localforage.js',*/
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function (event) {
+    postMsg({msg:'Updating...'});
+    var urlsAddVersion = urlsToCache.map(function (url) {
+        return url + '?ver=' + CACHE_NAME
+    });
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsAddVersion);
+            }).then(() => {
+                console.log('Cache downloaded')
+                self.skipWaiting()
+            })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => {
-        return response ? response : fetch(event.request);
-      })
-  );
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request, {
+            ignoreSearch: true
+        }).then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            console.log('cache miss', event.request.url)
+            return fetch(event.request);
+        })
+    );
 });
+
+
+self.addEventListener('activate', function (event) {
+    console.log('activated, remove unused cache...')
+    var cacheAllowlist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheAllowlist.indexOf(cacheName) === -1) {
+                        console.log(cacheName)
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    postMsg({msg:'Updated!'})
+});
+
+function postMsg(obj) {
+    clients.matchAll({ includeUncontrolled: true, type: 'window' }).then((arr) => {
+        for (client of arr) {
+            client.postMessage(obj);
+        }
+    })
+}
